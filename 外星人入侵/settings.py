30bab9bd2d41ee73b5c5c1e02c4d5dc710c29225
njustbyq <2042675095@qq.com>
2021-12-11 16:46:45 +0800

    update ship

--- 外星人入侵/settings.py
+++ 外星人入侵/settings.py
@@ -7,3 +7,9 @@ class Settings():
         self.screen_width = 800
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
+
+        # 子弹设置
+        
